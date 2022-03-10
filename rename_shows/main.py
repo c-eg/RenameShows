b70b18978b712ef83982df2e5ff0e404090517b8
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-03-10 23:50:45 +0000

    simple testing set up

--- rename_shows/main.py
+++ rename_shows/main.py
@@ -3,14 +3,14 @@ from util.show_info_matcher import ShowInfoMatcher
 
 def main():
     test = ShowInfoMatcher("The.Good.Place.S04E11.1080p.NF.WEB-DL.DDP5.1.x264-AJP69")
-    print(test.title)
-    print(test.year)
-    print(test.resolution)
+    print(test.to_dictionary())
+
+    print("\n")
 
     test = ShowInfoMatcher("First.Man.2018.1080p.BluRay.REMUX.AVC.Atmos-EPSiLON.torrent")
-    print(test.title)
-    print(test.year)
-    print(test.resolution)
+    print(test.to_dictionary())
+
+    print("\n")
 
 
 if __name__ == "__main__":
