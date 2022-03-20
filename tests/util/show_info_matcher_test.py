eedf368992e1dc4b07431b3c8f2b56d85d5920ef
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-03-20 18:58:19 +0000

    More tests

--- tests/util/show_info_matcher_test.py
+++ tests/util/show_info_matcher_test.py
@@ -10,7 +10,18 @@ class TestShowInfoMatcher(unittest.TestCase):
 
     """Title Tests"""
     def test_match_title(self):
-        self.assertTrue(False)
+        test_case = "some.random.movie.2010.1080p.blueray.x264"
+        show_info_matcher = ShowInfoMatcher(test_case)
+        expected_result = "some random movie"
+
+        self.assertEqual(show_info_matcher.title, expected_result, "")
+
+    def test_match_title_contianing_year(self):
+        test_case = "some.random.movie.2012.2010.1080p.blueray.x264"
+        show_info_matcher = ShowInfoMatcher(test_case)
+        expected_result = "some random movie 2012"
+
+        self.assertEqual(show_info_matcher.title, expected_result)
 
     """Year Tests"""
     def test_match_year(self):
@@ -43,44 +54,76 @@ class TestShowInfoMatcher(unittest.TestCase):
         self.assertEqual(show_info_matcher.resolution, expected_result)
 
     """Source Tests"""
-    def test_match_source(self):
-        self.assertTrue(False)
+    # def test_match_source_cam(self):
+    #     self.assertTrue(False)
 
-    """Video Codec Tests"""
-    def test_match_video_codec(self):
-        self.assertTrue(False)
+    # """Video Codec Tests"""
+    # def test_match_video_codec(self):
+    #     self.assertTrue(False)
 
     """Audio Tests"""
-    def test_match_audio(self):
-        self.assertTrue(False)
+    def test_match_audio_aac(self):
+        test_case = "some.random.movie.2010.1080p.blueray.aac.x264"
+        show_info_matcher = ShowInfoMatcher(test_case)
+        expected_result = "aac"
+
+        self.assertEqual(show_info_matcher.audio, expected_result)
+
+    def test_match_audio_aac2_0(self):
+        test_case = "some.random.movie.2010.1080p.blueray.aac2.0.x264"
+        show_info_matcher = ShowInfoMatcher(test_case)
+        expected_result = "aac2.0"
+
+        self.assertEqual(show_info_matcher.audio, expected_result)
+
+    def test_match_audio_ac3(self):
+        test_case = "some.random.movie.2010.1080p.blueray.ac3.x264"
+        show_info_matcher = ShowInfoMatcher(test_case)
+        expected_result = "ac3"
+
+        self.assertEqual(show_info_matcher.audio, expected_result)
+
+    def test_match_audio_dts(self):
+        test_case = "some.random.movie.2010.1080p.blueray.dts.x264"
+        show_info_matcher = ShowInfoMatcher(test_case)
+        expected_result = "dts"
+
+        self.assertEqual(show_info_matcher.audio, expected_result)
+
+    def test_match_audio_dd5_1(self):
+        test_case = "some.random.movie.2010.1080p.blueray.dd5.1.x264"
+        show_info_matcher = ShowInfoMatcher(test_case)
+        expected_result = "dd5.1"
+
+        self.assertEqual(show_info_matcher.audio, expected_result)
 
     """Language Tests"""
-    def test_match_language(self):
-        self.assertTrue(False)
+    # def test_match_language(self):
+    #     self.assertTrue(False)
 
     """Edition Tests"""
-    def test_match_edition(self):
-        self.assertTrue(False)
+    # def test_match_edition(self):
+    #     self.assertTrue(False)
 
     """Tags Tests"""
-    def test_match_tags(self):
-        self.assertTrue(False)
+    # def test_match_tags(self):
+    #     self.assertTrue(False)
 
     """Release Info Tests"""
-    def test_match_release_info(self):
-        self.assertTrue(False)
+    # def test_match_release_info(self):
+    #     self.assertTrue(False)
 
     """Season Tests"""
-    def test_match_season(self):
-        self.assertTrue(False)
+    # def test_match_season(self):
+    #     self.assertTrue(False)
 
     """Episode Tests"""
-    def test_match_episode(self):
-        self.assertTrue(False)
+    # def test_match_episode(self):
+    #     self.assertTrue(False)
 
     """Release Group Tests"""
-    def test_match_release_group(self):
-        self.assertTrue(False)
+    # def test_match_release_group(self):
+    #     self.assertTrue(False)
 
 if __name__ == "__main__":
     unittest.main()
