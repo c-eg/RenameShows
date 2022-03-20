e6b96f89e776ca054a22a63dbc5f707093627004
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-03-20 01:07:38 +0000

    setup boilerplate to test show_info_matcher

--- tests/util/show_info_matcher_test.py
+++ tests/util/show_info_matcher_test.py
@@ -5,12 +5,44 @@ from rename_shows.util.show_info_matcher import ShowInfoMatcher
 
 class TestShowInfoMatcher(unittest.TestCase):
 
-    def setUp(self):
-        self.show_info_matches = ShowInfoMatcher("First.Man.2018.1080p.BluRay.REMUX.AVC.Atmos-EPSiLON.torrent")
-        # pass
+    def test_match_title(self):
+        self.assertEqual(False)
 
-    def test_title_match(self):
-        self.assertEqual(self.show_info_matches.title, "First Man")
+    def test_match_year(self):
+        self.assertEqual(False)
+
+    def test_match_resolution(self):
+        self.assertEqual(False)
+
+    def test_match_source(self):
+        self.assertEqual(False)
+
+    def test_match_video_codec(self):
+        self.assertEqual(False)
+
+    def test_match_audio(self):
+        self.assertEqual(False)
+
+    def test_match_language(self):
+        self.assertEqual(False)
+
+    def test_match_edition(self):
+        self.assertEqual(False)
+
+    def test_match_tags(self):
+        self.assertEqual(False)
+
+    def test_match_release_info(self):
+        self.assertEqual(False)
+
+    def test_match_season(self):
+        self.assertEqual(False)
+
+    def test_match_episode(self):
+        self.assertEqual(False)
+
+    def test_match_release_group(self):
+        self.assertEqual(False)
 
 if __name__ == "__main__":
     unittest.main()
