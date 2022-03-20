a2799a08688ecb38a6a5675fabc5fb2638638d14
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-03-20 14:33:33 +0000

    Add comments

--- tests/util/show_info_matcher_test.py
+++ tests/util/show_info_matcher_test.py
@@ -1,5 +1,4 @@
 import unittest
-from cgi import test
 
 from rename_shows.util.show_info_matcher import ShowInfoMatcher
 
@@ -8,9 +7,12 @@ class TestShowInfoMatcher(unittest.TestCase):
     """
     Unit test class to test ShowInfoMatcher in rename_shows/util/show_info_matcher.
     """
+
+    """Title Tests"""
     def test_match_title(self):
         self.assertTrue(False)
 
+    """Year Tests"""
     def test_match_year(self):
         test_case = "some.random.movie.2010.1080p.blueray.x264"
         show_info_matcher = ShowInfoMatcher(test_case)
@@ -18,6 +20,7 @@ class TestShowInfoMatcher(unittest.TestCase):
 
         self.assertEqual(show_info_matcher.year, expected_result)
 
+    """Resolution Tests"""
     def test_match_resolution_480p(self):
         test_case = "some.random.movie.2010.480p.blueray.x264"
         show_info_matcher = ShowInfoMatcher(test_case)
@@ -39,33 +42,43 @@ class TestShowInfoMatcher(unittest.TestCase):
 
         self.assertEqual(show_info_matcher.resolution, expected_result)
 
+    """Source Tests"""
     def test_match_source(self):
         self.assertTrue(False)
 
+    """Video Codec Tests"""
     def test_match_video_codec(self):
         self.assertTrue(False)
 
+    """Audio Tests"""
     def test_match_audio(self):
         self.assertTrue(False)
 
+    """Language Tests"""
     def test_match_language(self):
         self.assertTrue(False)
 
+    """Edition Tests"""
     def test_match_edition(self):
         self.assertTrue(False)
 
+    """Tags Tests"""
     def test_match_tags(self):
         self.assertTrue(False)
 
+    """Release Info Tests"""
     def test_match_release_info(self):
         self.assertTrue(False)
 
+    """Season Tests"""
     def test_match_season(self):
         self.assertTrue(False)
 
+    """Episode Tests"""
     def test_match_episode(self):
         self.assertTrue(False)
 
+    """Release Group Tests"""
     def test_match_release_group(self):
         self.assertTrue(False)
 
