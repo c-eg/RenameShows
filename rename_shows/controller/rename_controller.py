3bac0f1d34aa2f155054a95106adc3fa94d58315
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-04-16 01:56:54 +0100

    Forgot copyright header

--- rename_shows/controller/rename_controller.py
+++ rename_shows/controller/rename_controller.py
@@ -1,7 +1,23 @@
+"""
+This file is part of RenameShows.
+
+RenameShows is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RenameShows is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RenameShows.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import glob
 import os
 
-import dotenv
 from rename_shows.api.the_movie_database_api import TheMovieDatabaseAPI
 from rename_shows.util.show_info_matcher import ShowInfoMatcher
 
