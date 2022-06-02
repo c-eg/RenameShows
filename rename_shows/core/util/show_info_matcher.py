219dda09f2c6f0fb82779b652fa30e66d9ca547b
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-06-02 02:22:26 +0100

    fix: change regex statements to python-regex convention, also fix #9

--- rename_shows/core/util/show_info_matcher.py
+++ rename_shows/core/util/show_info_matcher.py
@@ -87,7 +87,7 @@ class ShowInfoMatcher:
         """
         Matches the year of the show.
         """
-        regex = "[\\.\\s](?!^)[1,2]\\d{3}[\\.\\s]"
+        regex = r"[\.\s](?!^)[1,2]\d{3}[\.\s]"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -100,7 +100,7 @@ class ShowInfoMatcher:
         """
         Matches the resoltion of the show.
         """
-        regex = "\\d{3,4}p"
+        regex = r"\d{3,4}p"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -113,7 +113,7 @@ class ShowInfoMatcher:
         """
         Matches the source of the show.
         """
-        regex = "[\\.\\s](CAM|(DVD|BD)SCR|SCR|DDC|R5[\\.\\s]LINE|R5|(DVD|HD|BR|BD|WEB)Rip|DVDR|(HD|PD)TV|WEB-DL|WEBDL|BluRay|Blu-Ray|TS(?!C)|TELESYNC)"
+        regex = r"[\.\s](CAM|(DVD|BD)SCR|SCR|DDC|R5[\.\s]LINE|R5|(DVD|HD|BR|BD|WEB)Rip|DVDR|(HD|PD)TV|WEB-DL|WEBDL|BluRay|Blu-Ray|TS(?!C)|TELESYNC)"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -126,7 +126,7 @@ class ShowInfoMatcher:
         """
         Matches the video codec of the show.
         """
-        regex = "[\\.\\s](NTSC|PAL|[xh][\\.\\s]?264|[xh][\\.\\s]?265|H264|H265)"
+        regex = r"[\.\s](NTSC|PAL|[xh][\.\s]?264|[xh][\.\s]?265|H264|H265)"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -139,7 +139,7 @@ class ShowInfoMatcher:
         """
         Matches the audio of the show.
         """
-        regex = "AAC2[\\.\\s]0|AAC|AC3|DTS|DD5[\\.\\s]1"
+        regex = r"AAC2[\.\s]0|AAC|AC3|DTS|DD5[\.\s]1"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -152,7 +152,7 @@ class ShowInfoMatcher:
         """
         Matches the language of the show.
         """
-        regex = "[\\.\\s](MULTiSUBS|MULTi|NORDiC|DANiSH|SWEDiSH|NORWEGiAN|GERMAN|iTALiAN|FRENCH|SPANiSH)"
+        regex = r"[\.\s](MULTiSUBS|MULTi|NORDiC|DANiSH|SWEDiSH|NORWEGiAN|GERMAN|iTALiAN|FRENCH|SPANiSH)"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -165,7 +165,7 @@ class ShowInfoMatcher:
         """
         Matches the edition of the show.
         """
-        regex = "UNRATED|DC|(Directors|EXTENDED)[\\.\\s](CUT|EDITION)|EXTENDED|3D|2D|\\bNF\\b"
+        regex = r"UNRATED|DC|(Directors|EXTENDED)[\.\s](CUT|EDITION)|EXTENDED|3D|2D|\bNF\b"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -178,7 +178,7 @@ class ShowInfoMatcher:
         """
         Matches the tags of the show.
         """
-        regex = "COMPLETE|LiMiTED|iNTERNAL"
+        regex = r"COMPLETE|LiMiTED|iNTERNAL"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -191,7 +191,7 @@ class ShowInfoMatcher:
         """
         Matches the release info of the show.
         """
-        regex = "[\\.\\s](REAL[\\.\\s]PROPER|PROPER|REPACK|READNFO|READ[\\.\\s]NFO|DiRFiX|NFOFiX)"
+        regex = r"[\.\s](REAL[\.\s]PROPER|PROPER|REPACK|READNFO|READ[\.\s]NFO|DiRFiX|NFOFiX)"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -204,7 +204,7 @@ class ShowInfoMatcher:
         """
         Matches the season of the show.
         """
-        regex = "s(?:eason)?\\s*(\\d{1,2})"
+        regex = r"s(?:eason)?\s*(\d{1,2})"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
@@ -227,7 +227,8 @@ class ShowInfoMatcher:
         :return: tuple of episode numbers or None if none
         """
         regex = (
-            "e(?:pisode)?\\s*(\\d{1,3}(?!\\d)|\\d\\d\\d??)(?:-?e?(\\d{1,3}))?(?!\\d)"
+            # r"e(?:pisode)?\s*(\d{1,3}(?!\d)|\d\d\d??)(?:-?e?(\d{1,3}))?(?!\d)"
+            r"e(?:pisode\s*)?(\d{1,3}(?!\d)|\d\d\d??)(?:-?e?(\d{1,3}))?(?!\d)"
         )
         pattern = re.compile(regex, flags=re.IGNORECASE)
 
@@ -248,7 +249,7 @@ class ShowInfoMatcher:
         """
         Matches the release group of the show.
         """
-        regex = "- ?([^\\-. ]+)$"
+        regex = r"- ?([^\-. ]+)$"
         pattern = re.compile(regex, flags=re.IGNORECASE)
         matcher = pattern.search(self.__file_name)
 
