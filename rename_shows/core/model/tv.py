fd380a31e4d22acd0f89e711fbb1a5a60a14c8d9
Conor Egan <68134729+c-eg@users.noreply.github.com>
2022-07-02 23:27:36 +0100

    feat: add support for tv series

---
+++ rename_shows/core/model/tv.py
@@ -0,0 +1,25 @@
+"""
+This file is part of RenameShows.
+
+RenameShows is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RenameShows is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RenameShows.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from rename_shows.core.model.show import Show
+
+
+class Tv(Show):
+    """Class to represent a tv show."""
+
+    def __init__(self, title=None):
+        super().__init__(title)
